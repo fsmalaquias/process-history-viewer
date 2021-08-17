849140dbf3410e938e72c8fffa5c29f111faec49
fsmalaquias <malaquias.flavio@gmail.com>
2021-08-17 12:52:23 -0300

    Enumerators Created

---
+++ src/services/camunda.enum.ts
@@ -0,0 +1,19 @@
+enum SortBy {
+  InstanceId = 'instanceId',
+  DefinitionId = 'definitionId',
+  DefinitionKey = 'definitionKey',
+  DefinitionName = 'definitionName',
+  DefinitionVersion = 'definitionVersion',
+  BusinessKey = 'businessKey',
+  StartTime = 'startTime',
+  EndTime = 'endTime',
+  Duration = 'duration',
+  TenantId = 'tenantId'
+}
+
+enum SortOrder {
+  Ascending = 'asc',
+  Descending = 'desc'
+}
+
+export {SortBy, SortOrder}
--- src/services/camunda.routes.ts
+++ src/services/camunda.routes.ts
@@ -1,16 +1,16 @@
 
+import { SortBy, SortOrder } from "./camunda.enum";
+
 export default class CamundaRoutes{
   static BaseURL: string = String(process.env.REACT_APP_BASE_URL);
 
-  static getProcessInstancePath(sortBy: string = 'startTime', sortOrder: string = 'desc') {
+  static getProcessInstancePath(sortBy: string = SortBy.StartTime, sortOrder: string = SortOrder.Descending) {
     console.log('getProcessInstancePath', process.env);
     return `${this.BaseURL}/engine-rest/history/process-instance?sortBy=${sortBy}&sortOrder=${sortOrder}`;
   }
 
-  static getProcessInstanceActivityListPath(processInstanceId: string, sortBy: string = 'startTime', sortOrder: string = 'asc') {
+  static getProcessInstanceActivityListPath(processInstanceId: string, sortBy: string = SortBy.StartTime, sortOrder: string = SortOrder.Ascending) {
     return `${this.BaseURL}/engine-rest/history/activity-instance?processInstanceId=${processInstanceId}&sortBy=${sortBy}&sortOrder=${sortOrder}`;
   }
 
-  //TODO: Create Enums for sorting and ordering fields
-  //instanceId, definitionId, definitionKey, definitionName, definitionVersion, businessKey, startTime, endTime, duration and tenantId
 }
