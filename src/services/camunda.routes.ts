546de60967a109e23bcd4985454fbe658cd31bb8
fsmalaquias <malaquias.flavio@gmail.com>
2021-08-17 11:26:23 -0300

    Ajusts on sorting

--- src/services/camunda.routes.ts
+++ src/services/camunda.routes.ts
@@ -2,12 +2,15 @@
 export default class CamundaRoutes{
   static BaseURL: string = String(process.env.REACT_APP_BASE_URL);
 
-  static getProcessInstancePath() {
+  static getProcessInstancePath(sortBy: string = 'startTime', sortOrder: string = 'desc') {
     console.log('getProcessInstancePath', process.env);
-    return `${this.BaseURL}/engine-rest/history/process-instance`;
+    return `${this.BaseURL}/engine-rest/history/process-instance?sortBy=${sortBy}&sortOrder=${sortOrder}`;
   }
 
-  static getProcessInstanceActivityListPath(processInstanceId: string) {
-    return `${this.BaseURL}/engine-rest/history/activity-instance?processInstanceId=${processInstanceId}&sortBy=startTime&sortOrder=asc`;
+  static getProcessInstanceActivityListPath(processInstanceId: string, sortBy: string = 'startTime', sortOrder: string = 'asc') {
+    return `${this.BaseURL}/engine-rest/history/activity-instance?processInstanceId=${processInstanceId}&sortBy=${sortBy}&sortOrder=${sortOrder}`;
   }
+
+  //TODO: Create Enums for sorting and ordering fields
+  //instanceId, definitionId, definitionKey, definitionName, definitionVersion, businessKey, startTime, endTime, duration and tenantId
 }
